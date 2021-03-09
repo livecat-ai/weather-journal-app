339827757820fc6f7b28087fc38e3d3aa32233a2
Livecate <johnlockwood@me.com>
2021-03-09 21:23:47 +0000

    feat: Added function to get data from Openweathermap api

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+projectData =  {'date': '',
+                'temparature': '',
+                'userResponse': ''};
 
 // Require Express to run server and routes
+const express = require('express');
+const bodyParser = require('body-parser');
+// const { response } = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +17,27 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require("cors");
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
+const port = 3000;
 
 // Setup Server
+const server = app.listen(port, ()=>{console.log(`running on localhost: ${port}`)});
+
+app.get('/all', function (req, res) {
+    res.send(projectData);
+})
+
+app.post('/add', addData)
+
+function addData (req, res) {
+    console.log(req.body)
+    projectData.date = req.body.date;
+    projectData.temparature = req.body.temparature;
+    projectData.userResponse = req.body.userResponse;
+    res.send(projectData)
+}
--- website/app.js
+++ website/app.js
@@ -1,5 +1,82 @@
 /* Global Variables */
+const api_key = '7506705c269c4dfeddfb2d097984031d';
+let baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip='
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+
+// Post data
+const postData = async (url = '', data = {})=>{
+    // console.log(data);
+        const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+
+        try {
+            const newData = await response.json();
+            // console.log(newData);
+            return newData;
+        } catch(error) {
+            console.log("error", error);
+        }
+};
+
+// postData('/add', {date: 'data',
+//                   temparature: 79,
+//                   userResponse: 'Love warm weather'}); 
+
+
+// Fetch weather from Openweathermap api
+
+
+
+const getWeatherFromAPI = async (url = '', zip, api_key)=>{
+    // Make the call to the api capture the response
+    const fullURL = url + zip + '&units=metric' + '&APPID=' + api_key;
+    // console.log(fullURL);
+    const response = await fetch(fullURL, {
+        method: 'GET',
+        credentials: 'same-origin',
+    });
+    try {
+        const newData = await response.json();
+        // console.log(newData);
+        return newData;
+    } catch(error) {
+        console.log("error", error);
+    }
+};
+
+function GetPost(){
+    const zipcode = document.getElementById("zip").value;
+    const feelings = document.getElementById("feelings").value;
+    getWeatherFromAPI(baseURL, zipcode, api_key)
+        .then(function(data){
+            postData("/add", {"date": newDate,
+                            "temperature": data.main.temp,
+                            "feelings": feelings});
+        })
+        .then(function(data){
+
+        }
+        )
+};
+
+// Get zipcode from user
+
+const generateButton = document.getElementById("generate");
+
+generateButton.addEventListener('click', generateButtonClickEventCallback);
+
+function generateButtonClickEventCallback(event) {
+    // const weatherData = getWeather(baseURL, zipcode, api_key);
+    const weatherData = GetPost();
+    // console.log(weatherData);
+}
